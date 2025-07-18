fdc22ec059b0a6bdb0e397601d88adfa5b28b5d3
Ying Zhong <0x00eeee@gmail.com>
2025-07-18 21:05:10 +0800

    Simplify enabledMode with new markedit-theming

--- main.ts
+++ main.ts
@@ -1,16 +1,9 @@
-import { CustomTheme, overrideThemes } from 'markedit-theming';
-import { MarkEdit, type JSONObject, type JSONValue } from 'markedit-api';
+import { overrideThemes } from 'markedit-theming';
 import { tokyoNight } from '@uiw/codemirror-theme-tokyo-night';
 import { tokyoNightDay } from '@uiw/codemirror-theme-tokyo-night-day';
 
-const toObject = (value: JSONValue, fallback = {}) => (value ?? fallback) as JSONObject;
-const rootValue = toObject(toObject(MarkEdit.userSettings)['extension.markeditThemeTokyoNight']);
-const enabledMode = (rootValue.enabledMode ?? 'both') as string;
-
-const lightTheme: CustomTheme = { extension: tokyoNightDay };
-const darkTheme: CustomTheme = { extension: tokyoNight };
-
 overrideThemes({
-  light: ['both', 'light'].includes(enabledMode) ? lightTheme : undefined,
-  dark: ['both', 'dark'].includes(enabledMode) ? darkTheme : undefined,
+  light: { extension: tokyoNightDay },
+  dark: { extension: tokyoNight },
+  options: { settingsKey: 'extension.markeditThemeTokyoNight' },
 });
