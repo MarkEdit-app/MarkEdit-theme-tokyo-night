375acdaff11a0f4c8a0df12529a841c9fd8eed71
Ying Zhong <0x00eeee@gmail.com>
2025-07-22 21:31:07 +0800

    Integrate latest markedit-theming

--- main.ts
+++ main.ts
@@ -3,7 +3,19 @@ import { tokyoNight } from '@uiw/codemirror-theme-tokyo-night';
 import { tokyoNightDay } from '@uiw/codemirror-theme-tokyo-night-day';
 
 overrideThemes({
-  light: { extension: tokyoNightDay },
-  dark: { extension: tokyoNight },
-  options: { settingsKey: 'extension.markeditThemeTokyoNight' },
+  light: {
+    extension: tokyoNightDay,
+    colors: {
+      subtleEmphasis: true,
+    },
+  },
+  dark: {
+    extension: tokyoNight,
+    colors: {
+      subtleEmphasis: true,
+    },
+  },
+  options: {
+    settingsKey: 'extension.markeditThemeTokyoNight',
+  },
 });
